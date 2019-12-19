4f007b1c00540c7d3287989da2d6d8e38e204adf
steven1020545 <58941294+steven1020545@users.noreply.github.com>
2019-12-19 17:10:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ var leftPressed = false;
 var colorNum=3;
 var ballColor=["#ADFF2F","#FFD700","#87CEFA","#FFFFFF"];
 
-var brickOffsetTop = 60;
-var brickOffsetLeft = 60;
+var brickOffsetTop = 70;
+var brickOffsetLeft = 70;
 var brickPadding = 20;
 var brickRowCount = 3;
 var brickColumnCount = 5;
